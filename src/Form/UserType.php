e36eef8faa7d59326f5f924601466628f6131d84
AhmedHanfi <hanafi.ahmeddhia@esprit.tn>
2023-05-09 00:27:16 +0100

    first commit: uploading project

---
+++ src/Form/UserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\Compound;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+                'compound' => true
+            ])
+            ->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function($rolesAsArray){
+                    return count($rolesAsArray) ? $rolesAsArray[0] : null;
+                },
+                function($rolesAsString){
+                    return [$rolesAsString];
+                }
+            ))
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
