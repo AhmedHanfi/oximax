734dc023f91d4fb41020793d5d7e3d4c2f140759
gazifie2 <gaz.boubaker@gmail.com>
2023-08-31 18:43:18 +0100

    correction getAcceuil

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -148,7 +148,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     
     public function getResponsable(): ?Responsable
     {
-        return $this->acceuil;
+        return $this->responsable;
     }
 
     public function setResponsable(?Responsable $responsable): self
